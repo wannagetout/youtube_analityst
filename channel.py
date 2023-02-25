b7b503a7cc841ea16ebd8f10e78ff228d970a083
wannagetout <inv.moiseev@gmail.com>
2023-02-25 09:50:42 +0300

    Makes properties as methods and implemented json writer method

--- channel.py
+++ channel.py
@@ -6,10 +6,45 @@ from utils import api_client
 class Channel:
 
 	def __init__(self, id_: object):
-		self.id_ = id_
+		self.__id_ = id_
 
-	def get_channel(self):
+	def channel_info(self):
 		client = api_client()
-		channel = client.channels().list(id=self.id_, part='snippet, statistics').execute()
+		channel = client.channels().list(id=self.__id_, part='snippet, statistics').execute()
 		channel = json.dumps(channel, indent=2, ensure_ascii=False)
-		return channel
+		return json.loads(channel)
+
+	@staticmethod
+	def get_service():
+		return api_client()
+
+	@property
+	def title(self):
+		return self.channel_info()['items'][0]['snippet']['title']
+
+	@property
+	def description(self):
+		return self.channel_info()['items'][0]['snippet']['description']
+
+	@property
+	def url(self):
+		return self.channel_info()['items'][0]['thumbnails']['url']
+
+	@property
+	def subscribers_count(self):
+		return self.channel_info()['items'][0]['statistics']['subscriberCount']
+
+	@property
+	def video_count(self):
+		return self.channel_info()['items'][0]['statistics']['videoCount']
+
+	@property
+	def views_count(self):
+		return self.channel_info()['items'][0]['statistics']['viewCount']
+
+	def to_json(self, file_name):
+		with open(file_name+'.json', 'w', encoding='utf-8') as file:
+			info = json.dumps(self.channel_info())
+			for line in info:
+				file.write(line)
+
--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from channel import Channel
 
 if __name__ == '__main__':
 
-	channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'
+	channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
 
 	a = Channel(channel_id)
-	print(a.get_channel())
+	print(a.title)
+	print(a.__dict__)
+	print(a.channel_info())
+	print(a.get_service())
+	a.to_json('name')
+	print(a.title)
