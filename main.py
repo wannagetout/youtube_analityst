e9476db732121cf6b2df8bcabe318e8216e66655
wannagetout <inv.moiseev@gmail.com>
2023-03-14 06:50:53 +0300

    Added demo in main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from channel import Channel
-
+from video import Video, PLVideo
 
 if __name__ == '__main__':
 
 	channel_id_1 = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
 	channel_id_2 = 'UC1eFXmJNkjITxPFWTy6RsWg'
+	channel1 = Channel(channel_id_1)
+	channel2 = Channel(channel_id_2)
 
-	ch1 = Channel(channel_id_1)
-	ch2 = Channel(channel_id_2)
-
-	print(ch1)
-	print(ch2)
-	print(ch1 > ch2)
-	# print(ch1.subscribers_count)
-	# print(ch2.subscribers_count)
-	print(ch1 + ch2)
+	print(PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD'))
